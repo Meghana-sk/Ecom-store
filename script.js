4658772bd8e0ed90157c762abcc29566aae6ec1c
Meghana-sk <meghana.meghanask.sk@gmail.com>
2022-02-19 16:25:53 +0530

    docs: Add comments for methods

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ const openMenu = document.querySelector('.open-menu');
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
+/**
+ * Shows hamburger menu with options for small screens
+ * @param none
+ * @return none
+ */
 function show() {
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
 }
 
+/**
+ * Hides hamburger menu with options on close icon click
+ * @param none
+ * @return none
+ */
 function close() {
-    mainMenu.style.top = '-100%';
+    mainMenu.style.visibilty = 'none';
 }
