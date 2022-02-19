00e1c2644d291a9d4105feca150e2722dce5cc14
Meghana-sk <meghana.meghanask.sk@gmail.com>
2022-02-19 20:22:14 +0530

    feat: Add images for category section

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const mainMenu = document.querySelector('.main-menu');
 const closeMenu = document.querySelector('.close-menu');
 const openMenu = document.querySelector('.open-menu');
 
-
 openMenu.addEventListener('click', show);
 closeMenu.addEventListener('click', close);
 
