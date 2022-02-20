cb5a03cdf6a822b2dce0a610f7787d7b72eaf49c
Meghana-sk <meghana.meghanask.sk@gmail.com>
2022-02-20 14:13:45 +0530

    feat: Add filter section nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,14 @@
-const mainMenu = document.querySelector('.main-menu');
-const closeMenu = document.querySelector('.close-menu');
-const openMenu = document.querySelector('.open-menu');
+const mainMenu = document.querySelector(".main-menu");
+const closeMenu = document.querySelector(".close-menu");
+const openMenu = document.querySelector(".open-menu");
+const openFilterMenu = document.querySelector(".add-filter-btn");
+const filterSection = document.querySelector(".prod-filters");
+const closeFilter = document.querySelector(".close-filter");
 
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+openFilterMenu.addEventListener("click", showFilter);
+closeFilter.addEventListener("click", closeFilter);
 
 /**
  * Shows hamburger menu with options for small screens
@@ -11,8 +16,8 @@ closeMenu.addEventListener('click', close);
  * @return none
  */
 function show() {
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
+    mainMenu.style.display = "flex";
+    mainMenu.style.top = "0";
 }
 
 /**
@@ -21,5 +26,25 @@ function show() {
  * @return none
  */
 function close() {
-    mainMenu.style.top = '-100%';
+    mainMenu.style.top = "-100%";
+}
+
+/**
+ * Shows hamburger menu with options for small screens
+ * @param none
+ * @return none
+ */
+function showFilter() {
+    filterSection.style.display = "block";
+    filterSection.style.left = "0";
+    // filterSection.style.top = "0";
+}
+
+/**
+ * Hides hamburger menu with options on close icon click
+ * @param none
+ * @return none
+ */
+function closeFilter() {
+    filterSection.style.left = "-100%";
 }
