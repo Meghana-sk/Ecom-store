30003de666264c2a655c9a51f50469bfe09481ad
Meghana-sk <meghana.meghanask.sk@gmail.com>
2022-02-20 16:41:36 +0530

    fix: Side bar filter

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,9 @@
 const mainMenu = document.querySelector(".main-menu");
 const closeMenu = document.querySelector(".close-menu");
 const openMenu = document.querySelector(".open-menu");
-const openFilterMenu = document.querySelector(".add-filter-btn");
-const filterSection = document.querySelector(".prod-filters");
-const closeFilter = document.querySelector(".close-filter");
 
 openMenu.addEventListener("click", show);
 closeMenu.addEventListener("click", close);
-openFilterMenu.addEventListener("click", showFilter);
-closeFilter.addEventListener("click", closeFilter);
 
 /**
  * Shows hamburger menu with options for small screens
@@ -27,24 +22,4 @@ function show() {
  */
 function close() {
     mainMenu.style.top = "-100%";
-}
-
-/**
- * Shows hamburger menu with options for small screens
- * @param none
- * @return none
- */
-function showFilter() {
-    filterSection.style.display = "block";
-    filterSection.style.left = "0";
-    // filterSection.style.top = "0";
-}
-
-/**
- * Hides hamburger menu with options on close icon click
- * @param none
- * @return none
- */
-function closeFilter() {
-    filterSection.style.left = "-100%";
 }
